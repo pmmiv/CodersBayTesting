069412b3b52208949f75b6bae42c8dd45bf6ce01
unknown <pmmilamiv@gmail.com>
2018-02-07 11:40:02 -0600

    show account details on user login

--- logic.js
+++ logic.js
@@ -111,6 +111,40 @@ database.ref("/bidderData").on("value", function(snapshot) {
 });
 
 // --------------------------------------------------------------
+ initApp = function() {
+        firebase.auth().onAuthStateChanged(function(user) {
+          if (user) {
+            // User is signed in.
+            var displayName = user.displayName;
+            var email = user.email;
+            var emailVerified = user.emailVerified;
+            var photoURL = user.photoURL;
+            var uid = user.uid;
+            var phoneNumber = user.phoneNumber;
+            var providerData = user.providerData;
+            user.getIdToken().then(function(accessToken) {
+              document.getElementById('sign-in-status').textContent = 'Signed in';
+              document.getElementById('sign-in').textContent = 'Sign out';
+              document.getElementById('account-details').textContent = JSON.stringify({
+                displayName: displayName,
+                uid: uid,
+              }, null, '  ');
+            });
+          } else {
+            // User is signed out.
+            document.getElementById('sign-in-status').textContent = 'Signed out';
+            document.getElementById('sign-in').textContent = 'Sign in';
+            document.getElementById('account-details').textContent = 'null';
+          }
+        }, function(error) {
+          console.log(error);
+        });
+      };
+
+      window.addEventListener('load', function() {
+        initApp()
+      });
+// --------------------------------------------------------------
 // Whenever a user clicks the click button
 $("#submit-bid").on("click", function(event) {
   event.preventDefault();
