b1d0043c49f4f4b6796d0b125458364c24f09811
PMMIV <pmmilamiv@gmail.com>
2018-02-11 23:57:52 -0600

    console logging a firebase response take 2

--- logic.js
+++ logic.js
@@ -434,11 +434,11 @@ $(document).on('click', '.bookmarkRem', function(){
   console.log(this.dataset.id);
   var thisId = this.dataset.id;
   database.ref("/users/" + actUser.uid).once('value').then(function(dataSnapshot){
-  var newBkmkCards = dataSnapshot.val();
-  console.log(newBkmkCards[key]);
-  for (var key in newBkmkCards) {
+    var newBkmkCards = dataSnapshot.val();
+    for (var key in newBkmkCards) {
+      console.log(newBkmkCards[key]);
       if (newBkmkCards.hasOwnProperty(key) && newBkmkCards[key].storeId == thisId) {
-          dbRemove(newBkmkCards[key]);
+        dbRemove(newBkmkCards[key]);
       }
     }
   });
